4fb6bab421ef1db8cd882da8c639f29549746124
Jan-Otto Kröpke <mail@jkroepke.de>
2023-10-05 16:05:35 +0200

    provide linux packages for openvpn plugin
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- packaging/preremove.sh
+++ packaging/preremove.sh
@@ -3,5 +3,5 @@
 set -e
 
 if command -v systemctl >/dev/null 2>&1; then
-    systemctl stop --now openvpn-auth-oauth2
+    systemctl disable -q --now openvpn-auth-oauth2 > /dev/null 2>&1 || true
 fi
