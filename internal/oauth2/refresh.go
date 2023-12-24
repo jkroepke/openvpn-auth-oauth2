c8a2908e59f508d76e2b1fe0795003cc978f3fe7
Jan-Otto Kröpke <mail@jkroepke.de>
2023-12-24 14:09:27 +0100

    fix bench
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- internal/oauth2/refresh.go
+++ internal/oauth2/refresh.go
@@ -24,13 +24,15 @@ func (p *Provider) RefreshClientAuth(clientID uint64, logger *slog.Logger) (bool
 		return false, fmt.Errorf("error from token store: %w", err)
 	}
 
-	logger.Debug("initiate non-interactive authentication via refresh token")
+	logger.Info("initiate non-interactive authentication via refresh token")
 	ctx := logging.ToContext(context.Background(), log.NewZitadelLogger(logger))
 
 	newTokens, err := rp.RefreshTokens[*oidc.IDTokenClaims](ctx, p.RelyingParty, refreshToken, "", "")
 	if err != nil {
 		return false, fmt.Errorf("failed authentication via refresh token %w", err)
 	}
+
+	logger.Info("successful authenticate via refresh token")
 
 	if err = p.storage.Set(clientID, newTokens.RefreshToken); err != nil {
 		return true, fmt.Errorf("error from token store: %w", err)
