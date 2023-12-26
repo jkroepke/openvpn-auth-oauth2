cba51f67108795a66d7a7afd06986d738db58446
Jan-Otto Kröpke <mail@jkroepke.de>
2023-12-26 10:30:37 +0100

    Avoid unnecessary memory allocations in OpenVPN handler
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

---
+++ internal/state/bench_test.go
@@ -0,0 +1,39 @@
+package state_test
+
+import (
+	"testing"
+
+	"github.com/jkroepke/openvpn-auth-oauth2/internal/state"
+	"github.com/jkroepke/openvpn-auth-oauth2/pkg/testutils"
+)
+
+func BenchmarkState(b *testing.B) {
+	b.StopTimer()
+
+	encryptionKey := testutils.Secret
+	token := state.New(state.ClientIdentifier{Cid: 9223372036854775807, Kid: 2}, "127.0.0.1", "test")
+
+	b.StartTimer()
+
+	b.Run("encode", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = token.Encode(encryptionKey)
+		}
+
+		b.ReportAllocs()
+	})
+
+	b.StopTimer()
+	encodedToken := state.NewEncoded(token.Encoded())
+	b.StartTimer()
+
+	b.Run("decode", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = encodedToken.Decode(encryptionKey)
+		}
+
+		b.ReportAllocs()
+	})
+
+	b.StopTimer()
+}
