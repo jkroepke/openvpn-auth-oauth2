92c73cc980ad70fe53679be5389fea3c5b81b798
Jan-Otto Kröpke <mail@jkroepke.de>
2024-01-13 03:29:39 +0100

    Remove replaces
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- internal/config/decode_hooks_test.go
+++ internal/config/decode_hooks_test.go
@@ -7,7 +7,8 @@ import (
 	"text/template"
 
 	"github.com/jkroepke/openvpn-auth-oauth2/internal/config"
-	"github.com/mitchellh/mapstructure"
+
+	"github.com/go-viper/mapstructure/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
