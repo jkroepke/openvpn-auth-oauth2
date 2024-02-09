251c6af968f5d6f0ec2cac3dfdaea7a00c7fc391
Jan-Otto Kröpke <mail@jkroepke.de>
2024-02-09 09:16:03 +0100

    fix lint
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- cmd/daemon/root_test.go
+++ cmd/daemon/root_test.go
@@ -94,7 +94,9 @@ func TestExecuteConfigInvalid(t *testing.T) {
 }
 
 // TestExecuteConfigFileFound tests the main program logic of openvpn-auth-oauth2 with a valid config file.
-func TestExecuteConfigFileFound(t *testing.T) { //nolint: paralleltest
+// It sets up a resource server, a management interface and a client. It then starts the main program logic
+// nolint: paralleltest
+func TestExecuteConfigFileFound(t *testing.T) {
 	clientListener := testutils.TCPTestListener(t)
 	defer clientListener.Close()
 
