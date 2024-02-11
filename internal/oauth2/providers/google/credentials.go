cdb759b18cae25bd10d0032c961214d292ca1fd1
Jan-Otto Kröpke <mail@jkroepke.de>
2024-02-11 22:04:57 +0100

    TargetPrincipal is required on impersonate
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- internal/oauth2/providers/google/credentials.go
+++ internal/oauth2/providers/google/credentials.go
@@ -17,64 +17,51 @@ import (
 // If a service account config is provided in the configuration, it will be used.
 func getHTTPClient(ctx context.Context, conf config.Config) (*http.Client, error) {
 	var (
-		err error
-		ts  oauth2.TokenSource
+		err         error
+		credentials *google.Credentials
+		ts          oauth2.TokenSource
 	)
 
 	if conf.Provider.Google.ServiceAccountConfig != "" {
-		ts, err = getTokenSourceByServiceAccountConfig(ctx, conf)
+		credentials, err = google.CredentialsFromJSON(ctx, []byte(conf.Provider.Google.ServiceAccountConfig))
+		if err != nil {
+			return nil, fmt.Errorf("error reading google service account config: %w", err)
+		}
 	} else {
-		ts, err = getTokenSourceByDefaultApplicationCredentials(ctx, conf)
+		credentials, err = google.FindDefaultCredentials(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("failed to fetch application default credentials: %w", err)
+		}
 	}
 
-	if err != nil {
-		return nil, err
+	if conf.Provider.Google.ImpersonateAccount == "" {
+		jwtConfig, err := google.JWTConfigFromJSON(credentials.JSON, AdminDirectoryGroupReadonlyScope)
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse client secret file to config: %w", err)
+		}
+
+		if conf.Provider.Google.AdminEmail != "" {
+			jwtConfig.Subject = conf.Provider.Google.AdminEmail
+		}
+
+		ts = jwtConfig.TokenSource(ctx)
+	} else {
+		credentialsConfig := impersonate.CredentialsConfig{
+			Scopes:          []string{AdminDirectoryGroupReadonlyScope},
+			Lifetime:        300 * time.Second,
+			TargetPrincipal: conf.Provider.Google.ImpersonateAccount,
+		}
+
+		if conf.Provider.Google.AdminEmail != "" {
+			credentialsConfig.Subject = conf.Provider.Google.AdminEmail
+		}
+
+		ts, err = impersonate.CredentialsTokenSource(ctx, credentialsConfig, option.WithCredentials(credentials))
+
+		if err != nil {
+			return nil, fmt.Errorf("CredentialsTokenSource error: %w", err)
+		}
 	}
 
 	return oauth2.NewClient(ctx, ts), nil
-}
-
-func getTokenSourceByDefaultApplicationCredentials(ctx context.Context, conf config.Config) (oauth2.TokenSource, error) {
-	credentials, err := google.FindDefaultCredentials(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed to fetch application default credentials: %w", err)
-	}
-
-	credentialsConfig := impersonate.CredentialsConfig{
-		Scopes:   []string{AdminDirectoryGroupReadonlyScope},
-		Lifetime: 300 * time.Second,
-	}
-
-	if len(conf.Provider.Google.ImpersonateAccount) > 0 {
-		credentialsConfig.TargetPrincipal = conf.Provider.Google.ImpersonateAccount
-	}
-
-	if len(conf.Provider.Google.AdminEmail) > 0 {
-		credentialsConfig.Subject = conf.Provider.Google.AdminEmail
-	}
-
-	ts, err := impersonate.CredentialsTokenSource(ctx, credentialsConfig, option.WithCredentials(credentials))
-	if err != nil {
-		return nil, fmt.Errorf("CredentialsTokenSource error: %w", err)
-	}
-
-	return ts, nil
-}
-
-func getTokenSourceByServiceAccountConfig(ctx context.Context, conf config.Config) (oauth2.TokenSource, error) {
-	credentials, err := google.CredentialsFromJSON(ctx, []byte(conf.Provider.Google.ServiceAccountConfig))
-	if err != nil {
-		return nil, fmt.Errorf("error reading credentials: %w", err)
-	}
-
-	jwtConfig, err := google.JWTConfigFromJSON(credentials.JSON, AdminDirectoryGroupReadonlyScope)
-	if err != nil {
-		return nil, fmt.Errorf("unable to parse client secret file to config: %w", err)
-	}
-
-	if len(conf.Provider.Google.AdminEmail) > 0 {
-		jwtConfig.Subject = conf.Provider.Google.AdminEmail
-	}
-
-	return jwtConfig.TokenSource(ctx), err
 }
