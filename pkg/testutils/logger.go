d74742c4ab3080d1f8dfc1b7ca28fe8cbc7d323f
Jan-Otto Kröpke <mail@jkroepke.de>
2024-02-11 13:02:38 +0100

    fix tests
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- pkg/testutils/logger.go
+++ pkg/testutils/logger.go
@@ -3,15 +3,16 @@ package testutils
 import (
 	"bytes"
 	"log/slog"
+	"sync"
 )
 
 type Logger struct {
 	*slog.Logger
-	*bytes.Buffer
+	*Buffer
 }
 
 func NewTestLogger() *Logger {
-	buffer := new(bytes.Buffer)
+	buffer := new(Buffer)
 
 	return &Logger{
 		slog.New(slog.NewTextHandler(buffer, nil)),
@@ -21,4 +22,25 @@ func NewTestLogger() *Logger {
 
 func (l Logger) GetLogs() string {
 	return l.Buffer.String()
+}
+
+type Buffer struct {
+	buffer bytes.Buffer
+	mutex  sync.Mutex
+}
+
+// Write appends the contents of p to the buffer, growing the buffer as needed. It returns
+// the number of bytes written.
+func (s *Buffer) Write(p []byte) (n int, err error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	return s.buffer.Write(p)
+}
+
+// String returns the contents of the unread portion of the buffer
+// as a string.  If the Buffer is a nil pointer, it returns "<nil>".
+func (s *Buffer) String() string {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	return s.buffer.String()
 }
