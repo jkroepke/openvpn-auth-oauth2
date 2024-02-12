625a912120da50a0f8fc292ec581b61ccd2bb4fa
Pieter <110168856+Pionerd@users.noreply.github.com>
2024-02-12 17:53:26 +0100

    Update preremove.sh
    Signed-off-by: Pieter <110168856+Pionerd@users.noreply.github.com>

--- packaging/preremove.sh
+++ packaging/preremove.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ -x "$(command -v deluser)" ]; then
-     deluser --quiet --system _openvpn-auth-oauth2 > /dev/null || true
+     deluser --quiet --system openvpn-auth-oauth2 > /dev/null || true
   else
-     echo >&2 "not removing _openvpn-auth-oauth2 system account because deluser command was not found"
+     echo >&2 "not removing openvpn-auth-oauth2 system account because deluser command was not found"
 fi
 
 if command -v systemctl >/dev/null 2>&1; then
