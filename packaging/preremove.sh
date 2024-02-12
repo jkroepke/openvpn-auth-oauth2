1dbbc4956660543853ce3d070502c919ff92fa34
Pieter <110168856+Pionerd@users.noreply.github.com>
2024-02-12 17:51:22 +0100

    Naming convention
    Signed-off-by: Pieter <110168856+Pionerd@users.noreply.github.com>

--- packaging/preremove.sh
+++ packaging/preremove.sh
@@ -3,11 +3,11 @@
 set -e
 
 if [ -x "$(command -v deluser)" ]; then
-     deluser --quiet --system openvpn-auth-oauth2 > /dev/null || true
+     deluser --quiet --system _openvpn-auth-oauth2 > /dev/null || true
   else
-     echo >&2 "not removing openvpn-auth-oauth2 system account because deluser command was not found"
+     echo >&2 "not removing _openvpn-auth-oauth2 system account because deluser command was not found"
 fi
 
 if command -v systemctl >/dev/null 2>&1; then
-    systemctl disable -q --now openvpn-auth-oauth2 > /dev/null 2>&1 || true
+    systemctl disable -q --now _openvpn-auth-oauth2 > /dev/null 2>&1 || true
 fi
