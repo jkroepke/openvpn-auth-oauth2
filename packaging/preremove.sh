053594f54e8b45bc0d8daae2145efcd54572428a
Pieter <110168856+Pionerd@users.noreply.github.com>
2024-02-12 17:50:10 +0100

    Delete user
    Signed-off-by: Pieter <110168856+Pionerd@users.noreply.github.com>

--- packaging/preremove.sh
+++ packaging/preremove.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [ -x "$(command -v deluser)" ]; then
+     deluser --quiet --system openvpn-auth-oauth2 > /dev/null || true
+  else
+     echo >&2 "not removing openvpn-auth-oauth2 system account because deluser command was not found"
+fi
+
 if command -v systemctl >/dev/null 2>&1; then
     systemctl disable -q --now openvpn-auth-oauth2 > /dev/null 2>&1 || true
 fi
