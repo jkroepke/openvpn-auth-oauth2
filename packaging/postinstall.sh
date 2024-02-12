bfaae5f9c7a5cda12d0a6654d7d06cbaa84950b4
Pieter <110168856+Pionerd@users.noreply.github.com>
2024-02-12 17:51:54 +0100

    Naming convention
    Signed-off-by: Pieter <110168856+Pionerd@users.noreply.github.com>

--- packaging/postinstall.sh
+++ packaging/postinstall.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if ! getent passwd openvpn-auth-oauth2 > /dev/null; then
-    adduser --system --group --no-create-home --disabled-login --disabled-password openvpn-auth-oauth2
+if ! getent passwd _openvpn-auth-oauth2 > /dev/null; then
+    adduser --system --group --no-create-home --disabled-login --disabled-password _openvpn-auth-oauth2
 fi
 
 if command -v systemctl >/dev/null 2>&1; then
