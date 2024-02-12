c2e6d84964ec614068e985bffec61dc0c0dbf81e
Pieter <110168856+Pionerd@users.noreply.github.com>
2024-02-12 17:46:21 +0100

    Create user
    Signed-off-by: Pieter <110168856+Pionerd@users.noreply.github.com>

--- packaging/postinstall.sh
+++ packaging/postinstall.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if ! getent passwd openvpn-auth-oauth2 > /dev/null; then
+    adduser --system --group --no-create-home --disabled-login --disabled-password openvpn-auth-oauth2
+fi
+
 if command -v systemctl >/dev/null 2>&1; then
     systemctl daemon-reload
     if systemctl is-active --quiet openvpn-auth-oauth2; then
