8857962e01717821d4fafd5332e81298de82e5f6
Jan-Otto Kröpke <mail@jkroepke.de>
2024-03-18 08:10:17 +0100

    use context for cancel
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- internal/openvpn/passthrough.go
+++ internal/openvpn/passthrough.go
@@ -24,7 +24,7 @@ const writeTimeout = 20 * time.Millisecond
 // The passthrough interface is disabled by default. To enable it, set the passthrough.enabled option to true in the
 // configuration file.
 //
-//nolint:cyclop,gocognit
+//nolint:cyclop
 func (c *Client) handlePassthrough() {
 	var conn net.Conn
 
