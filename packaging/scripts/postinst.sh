a28ef95e2d0582effe5e62ebbdcc8694110c5d0c
Jan-Otto Kröpke <mail@jkroepke.de>
2024-03-29 14:42:25 +0100

    DEB/RPM: Do not remove system group on upgrade
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- packaging/scripts/postinst.sh
+++ packaging/scripts/postinst.sh
@@ -15,11 +15,11 @@ fi
 systemctl --system daemon-reload >/dev/null || true
 
 if systemctl is-active --quiet openvpn-auth-oauth2; then
-    systemctl restart openvpn-auth-oauth2 >/dev/null || true
+  systemctl restart openvpn-auth-oauth2 >/dev/null || true
 fi
 
 systemd-sysusers /usr/lib/sysusers.d/openvpn-auth-oauth2.conf >/dev/null || true
 
 if [ -d /etc/openvpn-auth-oauth2 ]; then
-    chown -R root:openvpn-auth-oauth2 /etc/openvpn-auth-oauth2/ >/dev/null || true
+  chown -R root:openvpn-auth-oauth2 /etc/openvpn-auth-oauth2/ >/dev/null || true
 fi
--- packaging/scripts/preremove.sh
+++ packaging/scripts/preremove.sh
@@ -2,16 +2,25 @@
 
 set -e
 
-if command -v apparmor_parser >/dev/null 2>&1 && command -v aa-enabled >/dev/null 2>&1; then
-  if aa-enabled --quiet 2>/dev/null; then
-    apparmor_parser --remove -T -W /etc/apparmor.d/usr.bin.openvpn-auth-oauth2 || true
+case $1 in
+1 | upgrade | failed-upgrade)
+  # RPM: If the first argument is 1, it means the package is being upgraded. https://docs.fedoraproject.org/en-US/packaging-guidelines/Scriptlets/#_syntax
+  # DEB: If the first argument is upgrade or failed-upgrade, it means the package is being upgraded. https://www.debian.org/doc/debian-policy/ch-maintainerscripts.html#summary-of-ways-maintainer-scripts-are-called
+  ;;
+
+*)
+  if command -v apparmor_parser >/dev/null 2>&1 && command -v aa-enabled >/dev/null 2>&1; then
+    if aa-enabled --quiet 2>/dev/null; then
+      apparmor_parser --remove -T -W /etc/apparmor.d/usr.bin.openvpn-auth-oauth2 || true
+    fi
   fi
-fi
 
-if ! command -v systemctl >/dev/null 2>&1; then
-  exit 0
-fi
+  if ! command -v systemctl >/dev/null 2>&1; then
+    exit 0
+  fi
 
-systemctl stop openvpn-auth-oauth2.service || true
-systemctl daemon-reload
-systemctl reset-failed
+  systemctl stop openvpn-auth-oauth2.service || true
+  systemctl daemon-reload
+  systemctl reset-failed
+  ;;
+esac
