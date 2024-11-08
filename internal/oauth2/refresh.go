c0de6ee0e00bcca0b145aaaf03ab3b1dfaac418d
Jan-Otto Kröpke <mail@jkroepke.de>
2024-11-08 19:35:54 +0100

    wip
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- internal/oauth2/refresh.go
+++ internal/oauth2/refresh.go
@@ -54,7 +54,10 @@ func (p *Provider) RefreshClientAuth(logger *slog.Logger, client connection.Clie
 		return false, fmt.Errorf("error from non-interactive authentication via refresh token: %w", err)
 	}
 
-	session := state.New(state.ClientIdentifier{CID: client.CID, KID: client.KID, SessionID: client.SessionID}, client.IPAddr, client.IPPort, client.CommonName, client.SessionState)
+	session := state.New(
+		state.ClientIdentifier{CID: client.CID, KID: client.KID, SessionID: client.SessionID},
+		client.IPAddr, client.IPPort, client.CommonName, client.SessionState,
+	)
 
 	user, err := p.Provider.GetUser(ctx, logger, tokens)
 	if err != nil {
