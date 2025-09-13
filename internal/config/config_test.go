1f541044cd4df136b3f4a326016d7e999ef02451
Jan-Otto Kröpke <mail@jkroepke.de>
2025-09-13 14:23:04 +0200

    config: openvpn.bypass.common-names accepts regular expressions now
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -20,6 +20,7 @@ import (
 	"golang.org/x/oauth2"
 )
 
+//goland:noinspection RegExpUnnecessaryNonCapturingGroup
 func TestConfig(t *testing.T) {
 	t.Parallel()
 
@@ -178,7 +179,7 @@ http:
 						OmitHost: false,
 					}},
 					Bypass: config.OpenVPNBypass{
-						CommonNames: types.RegexpSlice{regexp.MustCompile(`^test$`), regexp.MustCompile(`^test2$`)},
+						CommonNames: types.RegexpSlice{regexp.MustCompile(`^(?:test)$`), regexp.MustCompile(`^(?:test2)$`)},
 					},
 					ClientConfig: config.OpenVPNConfig{
 						Enabled:    true,
@@ -323,7 +324,8 @@ func TestConfigFlagSet(t *testing.T) {
 			[]string{"--openvpn.bypass.common-names=a,b"},
 			func() config.Config {
 				conf := config.Defaults
-				conf.OpenVPN.Bypass.CommonNames = types.RegexpSlice{regexp.MustCompile("a"), regexp.MustCompile("a")}
+				//goland:noinspection RegExpUnnecessaryNonCapturingGroup
+				conf.OpenVPN.Bypass.CommonNames = types.RegexpSlice{regexp.MustCompile("^(?:a)$"), regexp.MustCompile("^(?:b)$")}
 
 				return conf
 			}(),
