d816da279c37ff97525590247c898084a03f019b
Jan-Otto Kröpke <mail@jkroepke.de>
2025-11-23 18:02:51 +0100

    chore: add more plugin tests
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- lib/openvpn-auth-oauth2/management/management_test.go
+++ lib/openvpn-auth-oauth2/management/management_test.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"log/slog"
 	"net"
+	"strconv"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/jkroepke/openvpn-auth-oauth2/internal/openvpn"
 	"github.com/jkroepke/openvpn-auth-oauth2/internal/utils/testutils"
@@ -138,4 +141,96 @@ func TestServer_Listen_Password(t *testing.T) {
 	testutils.ExpectMessage(t, client, clientReader, "SUCCESS: password is correct")
 	testutils.ExpectMessage(t, client, clientReader, openvpn.WelcomeBanner)
 	testutils.SendMessagef(t, client, "quit")
+}
+
+func TestServer_AuthPendingPoller(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range []struct {
+		name    string
+		command string
+		testFn  func(t *testing.T, response *management.Response)
+	}{
+		{
+			name:    "client-auth-nt",
+			command: "client-auth-nt 1 0",
+			testFn: func(t *testing.T, response *management.Response) {
+				require.Equal(t, uint32(1), response.ClientID)
+				require.Equal(t, management.ClientAuthAccept, response.ClientAuth)
+			},
+		},
+		{
+			name:    "client-auth",
+			command: "client-auth 2 0\r\npush \"reneg-sec 0\"\r\nEND",
+			testFn: func(t *testing.T, response *management.Response) {
+				require.Equal(t, uint32(2), response.ClientID)
+				require.Equal(t, management.ClientAuthAccept, response.ClientAuth)
+				require.Equal(t, "push \"reneg-sec 0\"", response.ClientConfig)
+			},
+		},
+		{
+			name:    "client-deny",
+			command: "client-deny 3 0",
+			testFn: func(t *testing.T, response *management.Response) {
+				require.Equal(t, uint32(3), response.ClientID)
+				require.Equal(t, management.ClientAuthDeny, response.ClientAuth)
+			},
+		},
+		{
+			name:    "client-pending-auth",
+			command: "client-pending-auth 4 0 \"WEB_AUTH::https://sso.example.com/auth?session=xyz\" 300",
+			testFn: func(t *testing.T, response *management.Response) {
+				require.Equal(t, uint32(4), response.ClientID)
+				require.Equal(t, management.ClientAuthPending, response.ClientAuth)
+				require.Equal(t, "WEB_AUTH::https://sso.example.com/auth?session=xyz", response.Message)
+				require.Equal(t, "300", response.Timeout)
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			managementInterface, err := nettest.NewLocalListener("tcp")
+			require.NoError(t, err)
+
+			err = managementInterface.Close()
+			require.NoError(t, err)
+
+			managementServer := management.NewServer(slog.New(slog.DiscardHandler), "")
+			err = managementServer.Listen(t.Context(), fmt.Sprintf("%s://%s", managementInterface.Addr().Network(), managementInterface.Addr().String()))
+			require.NoError(t, err)
+
+			t.Cleanup(managementServer.Close)
+
+			responseCh := make(chan *management.Response, 1)
+			errCh := make(chan error, 1)
+
+			clientID, err := strconv.ParseUint(strings.Split(tc.command, " ")[1], 10, 64)
+			require.NoError(t, err)
+
+			go func() {
+				response, err := managementServer.AuthPendingPoller(clientID, time.Second*5)
+
+				errCh <- err
+				responseCh <- response
+			}()
+
+			var dialer net.Dialer
+
+			client, err := dialer.DialContext(t.Context(), "tcp", managementInterface.Addr().String())
+			require.NoError(t, err)
+
+			clientReader := bufio.NewReader(client)
+
+			testutils.ExpectMessage(t, client, clientReader, openvpn.WelcomeBanner)
+			testutils.SendMessagef(t, client, tc.command)
+			testutils.ExpectMessage(t, client, clientReader, fmt.Sprintf("SUCCESS: %s command succeeded", strings.TrimSuffix(strings.Split(tc.command, " ")[0], "-nt")))
+
+			require.NoError(t, <-errCh)
+			response := <-responseCh
+			require.NotNil(t, response)
+
+			tc.testFn(t, response)
+		})
+	}
 }
