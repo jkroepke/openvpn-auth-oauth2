7e094ecbee573f09ca07c9a5253c8041b58c41cb
Jan-Otto Kröpke <mail@jkroepke.de>
2025-12-26 16:52:55 +0100

    fix: explicitly enable httpmuxgo121 (#706)

--- cmd/openvpn-auth-oauth2/root.go
+++ cmd/openvpn-auth-oauth2/root.go
@@ -1,3 +1,7 @@
+// Explicitly disable httpmuxgo121, because Debian build system disables it.
+// ref: https://github.com/jkroepke/openvpn-auth-oauth2/issues/680#issuecomment-3686988447
+//go:debug httpmuxgo121=0
+
 package main
 
 import (
