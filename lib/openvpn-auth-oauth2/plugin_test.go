96f75cf3c2faa688f678914a869f04b61c5a6d38
Jan-Otto Kröpke <mail@jkroepke.de>
2026-03-15 10:05:19 +0100

    chore: plugin fixes
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- lib/openvpn-auth-oauth2/plugin_test.go
+++ lib/openvpn-auth-oauth2/plugin_test.go
@@ -21,3 +21,76 @@ func TestSelectInitializationPoint(t *testing.T) {
 
 	require.Equal(t, c.OpenVPNPluginInitPreDaemon, int(openvpn_plugin_select_initialization_point_v1()))
 }
+
+func TestPluginInvalidHandle(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range []struct {
+		name string
+		fn   func()
+	}{
+		{
+			"openvpn_plugin_func_v3_go_struct_ver_0",
+			func() {
+				openvpn_plugin_func_v3_go(0, nil, nil)
+			},
+		},
+		{
+			"openvpn_plugin_func_v3_go_args_nil",
+			func() {
+				openvpn_plugin_func_v3_go(openvpn.PluginStructVerMin, nil, nil)
+			},
+		},
+		{
+			"openvpn_plugin_func_v3_go_ret_nil",
+			func() {
+				openvpn_plugin_func_v3_go(openvpn.PluginStructVerMin,
+					&c.OpenVPNPluginArgsFuncIn{
+						Handle: nil,
+					},
+					nil)
+			},
+		},
+		{
+			"openvpn_plugin_func_v3_go_handle_nil",
+			func() {
+				openvpn_plugin_func_v3_go(openvpn.PluginStructVerMin,
+					&c.OpenVPNPluginArgsFuncIn{
+						Handle: nil,
+					},
+					&c.OpenVPNPluginArgsFuncReturn{},
+				)
+			},
+		},
+		{
+			"openvpn_plugin_close_v1",
+			func() {
+				openvpn_plugin_close_v1(nil)
+			},
+		},
+		{
+			"openvpn_plugin_client_constructor_v1",
+			func() {
+				openvpn_plugin_client_constructor_v1(nil)
+			},
+		},
+		{
+			"openvpn_plugin_client_destructor_v1",
+			func() {
+				openvpn_plugin_client_destructor_v1(nil, nil)
+			},
+		},
+		{
+			"openvpn_plugin_abort_v1",
+			func() {
+				openvpn_plugin_abort_v1(nil)
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			require.NotPanics(t, func() {
+				tc.fn()
+			})
+		})
+	}
+}
