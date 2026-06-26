a3b5e7f404416e94b99b4cbfef3b763b3515c27f
Jan-Otto Kröpke <mail@jkroepke.de>
2026-06-26 14:52:05 +0200

    fix(plugin): set plugin memory limit during plugin open (#934)
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- lib/openvpn-auth-oauth2/main.go
+++ lib/openvpn-auth-oauth2/main.go
@@ -50,6 +50,9 @@ func main() {
 	// This function is here to satisfy Go's requirement of having a main function.
 	// The main functionality is implemented in the openvpn_plugin_open_v3 function,
 	// which will be called from C.
+}
 
+//nolint:gochecknoinits
+func init() {
 	debug.SetMemoryLimit(memoryLimit)
 }
