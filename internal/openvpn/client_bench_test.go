368381562166c7ed23642c45f7f3a136b9283358
Jan-Otto Kröpke <mail@jkroepke.de>
2026-08-01 21:32:57 +0200

    chore(openvpn): reduce pending-auth command allocations
    Replace the successful-path fmt.Sprintf call with typed numeric formatting and a pre-sized strings.Builder. This removes interface-boxing escapes while preserving command formatting and the OpenVPN byte limit.
    
    goos: darwin
    goarch: arm64
    pkg: github.com/jkroepke/openvpn-auth-oauth2/v2/internal/openvpn
    cpu: Apple M1
                                    │     old     │                 new                 │
                                    │   sec/op    │   sec/op     vs base                │
    BuildClientPendingAuthCommand-8   258.1n ± 4%   118.3n ± 4%  -54.16% (p=0.000 n=10)
    
                                    │    old     │                new                 │
                                    │    B/op    │    B/op     vs base                │
    BuildClientPendingAuthCommand-8   392.0 ± 0%   352.0 ± 0%  -10.20% (p=0.000 n=10)
    
                                    │    old     │                new                 │
                                    │ allocs/op  │ allocs/op   vs base                │
    BuildClientPendingAuthCommand-8   5.000 ± 0%   1.000 ± 0%  -80.00% (p=0.000 n=10)
    
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- internal/openvpn/client_bench_test.go
+++ internal/openvpn/client_bench_test.go
@@ -3,6 +3,9 @@ package openvpn //nolint:testpackage
 import (
 	"strings"
 	"testing"
+	"time"
+
+	"github.com/jkroepke/openvpn-auth-oauth2/v2/internal/openvpn/connection"
 )
 
 func BenchmarkBuildOAuth2StartURL(b *testing.B) {
@@ -17,4 +20,26 @@ func BenchmarkBuildOAuth2StartURL(b *testing.B) {
 	}
 
 	_ = startURL
+}
+
+func BenchmarkBuildClientPendingAuthCommand(b *testing.B) {
+	client := connection.Client{CID: 12345, KID: 67890}
+	startURL := "https://vpn.example.com/oauth2/start?state=" + strings.Repeat("a", 256)
+
+	var (
+		command string
+		err     error
+	)
+
+	b.ReportAllocs()
+
+	for b.Loop() {
+		command, err = buildClientPendingAuthCommand(client, startURL, 300*time.Second)
+	}
+
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	_ = command
 }
